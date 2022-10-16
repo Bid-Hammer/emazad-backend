9c1e320f347c08236f53c05aa53cbbfd7a304ec3
Yaser Odat <yaserodat@outlook.com>
2022-10-16 12:39:35 +0000

    added item and bid models

--- models/bid.model.js
+++ models/bid.model.js
@@ -1,1 +1,27 @@
 'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+
+const Bid = sequelize.define('Bid', {
+  userID: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+
+  itemID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+
+  bidprice: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+
+})
+ 
+return Bid;
+}
+
+
+// userid, itemid, bidprice
