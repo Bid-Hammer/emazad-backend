f647344ad63f3339532cd6cebb5f094808d7dfa3
Omar Qattam <omarqattam99@gmail.com>
2022-10-16 12:50:48 +0000

    models report

--- models/report.model.js
+++ models/report.model.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+
+module.exports = (sequelize, DataTypes) => {
+    const Report =  sequelize.define('Report', {
+        reportTitle: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        reportMessage: {
+            type: DataTypes.STRING,    
+            allowNull: false
+        },
+        reportReason: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        itemID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    })
+    return Report;
+  }
