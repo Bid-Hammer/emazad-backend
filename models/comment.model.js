e2495521a1ed8d601744eca33c94c72e27023a38
qais-alsgher <qaisalsgher@gmail.com>
2022-10-16 12:42:23 +0000

    create schema for notification and comment and faverite

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,1 +1,19 @@
 'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Comment = sequelize.define('Comment', {
+        userID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        itemID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    })
+    return Comment;
+};
--- models/notification.model.js
+++ models/notification.model.js
@@ -1,1 +1,20 @@
 'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Notification = sequelize.define('Notification', {
+        userID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        itemID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        notiMessage: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+
+    })
+    return Notification;
+};
