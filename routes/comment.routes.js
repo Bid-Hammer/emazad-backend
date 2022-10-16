a58aad40044dfece23f5a51cb1d04d455cb0754c
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-10-16 22:38:43 +0300

    create comment, favorite, notifaction, rating route

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const Comment = require('../models/comment.model');
+
+router.get('/comment', getComments);
+router.get('/comment/:id', getCommentById);
+router.post('/comment', createComment);
+router.put('/comment/:id', updateComment);
+router.delete('/comment/:id', deleteComment);
+
+async function getComments(req, res) {
+  try {
+    const comments = await Comment.read();
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function getCommentById(req, res) {
+  try {
+    const id = req.params.id;
+    const comment = await Comment.read(id);
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function createComment(req, res) {
+  try {
+    const comment = await Comment.create(req.body);
+    res.status(201).json(comment);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function updateComment(req, res) {
+  try {
+    const id = req.params.id;
+    const comment = await Comment.update(id, req.body);
+    res.status(202).json(comment);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function deleteComment(req, res) {
+  try {
+    const id = req.params.id;
+    const comment = await Comment.delete(id);
+    res.status(204).json(comment);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+module.exports = router;
--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -1,1 +1,62 @@
-'use strict';
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const Rating = require("../models/rating.model");
+
+router.get("/rating", getRatings);
+router.get("/rating/:id", getRatingById);
+router.post("/rating", createRating);
+router.put("/rating/:id", updateRating);
+router.delete("/rating/:id", deleteRating);
+
+async function getRatings(req, res) {
+  try {
+    const ratings = await Rating.read();
+    res.status(200).json(ratings);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function getRatingById(req, res) {
+  try {
+    const id = req.params.id;
+    const rating = await Rating.read(id);
+    res.status(200).json(rating);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function createRating(req, res) {
+  try {
+    const rating = await Rating.create(req.body);
+    res.status(201).json(rating);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function updateRating(req, res) {
+  try {
+    const id = req.params.id;
+    const rating = await Rating.update(id, req.body);
+    res.status(202).json(rating);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function deleteRating(req, res) {
+  try {
+    const id = req.params.id;
+    const rating = await Rating.delete(id);
+    res.status(204).json(rating);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+module.exports = router;
