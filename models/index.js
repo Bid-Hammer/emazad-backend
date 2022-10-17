e48c16b059d92cfdcb048b9869dfc662b8e70a12
saeedkokash <saeedkokash@gmail.com>
2022-10-17 22:45:36 +0300

    added acl and created a comment test

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const collection = require("../collection/general-routes");
 
 // const user = require('./user.model');
-const POSTGRES_URL = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
+// const POSTGRES_URL = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
 // const sequelizeOption = {
 //     dialectOptions: {
 //         ssl: {
@@ -16,7 +16,7 @@ const POSTGRES_URL = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
 // for local testing
 
 // const POSTGRES_URL = 'postgres://odat:0000@localhost:5432/postgres';
-// const POSTGRES_URL = "postgresql://skokash:1094@localhost:5432/emazad";
+const POSTGRES_URL = "postgresql://skokash:1094@localhost:5432/emazad";
 
 const sequelizeOption = {};
 
