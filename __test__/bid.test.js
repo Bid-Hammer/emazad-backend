8ed29099a01f30fa40e2f913a30e35d8586cf680
Yaser Odat <yaserodat@outlook.com>
2022-10-17 18:34:58 +0000

    added tests

--- __test__/bid.test.js
+++ __test__/bid.test.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const server = require('../server');
+const supertest = require('supertest');
+const request = supertest(server.app);
+
+describe('Bid Test', () => {
+it('It can create a new bid', async () => {
+    const response = await request.post('/bid').send({
+        "userID": 1,
+        "itemID": 1,
+        "bidAmount": 20
+    });
+    expect(response.status).toEqual(201);
+});
+});
--- __test__/favorite.test.js
+++ __test__/favorite.test.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const server = require('../server');
+const supertest = require('supertest');
+const request = supertest(server.app);
+
+describe('Favorite Test', () => {
+
+});
